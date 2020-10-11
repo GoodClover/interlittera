e9e6cd8a89fa0abd1503b77e392453df22bf450f
GoodClover <oliversimmo@gmail.com>
2020-10-11 01:21:08 +0100

    Update create_zipapp.py

--- create_zipapp.py
+++ create_zipapp.py
@@ -1,9 +1,31 @@
 import zipapp
 from pathlib import Path
+from time import time
 
-source_dir = Path(__file__).parent
-dest_file = source_dir/"dist/interlittera.pyz"
 
-print(f"Creating zipapp from \"{source_dir}\" to \"{dest_file}\" ...")
+print("\nGetting directories... ", end="")
+t = time()
 
-zipapp.create_archive(source_dir, dest_file)
+root = Path(__file__).parent
+src = root / "src"
+dist = root / "dist"
+dist.mkdir(exist_ok=True)  # Make `dist` directory if it dosen't exist.
+dest_file = dist / "interlittera.pyz"
+i = 0
+while dest_file.exists():
+    i += 1
+    dest_file = dist / f"interlittera_{i}.pyz"
+
+
+t = time()-t
+print(f"done. {int(t*1000)}ms")
+print(f" {src=}")
+print(f" {dest_file=}")
+
+print(f"\nCreating zipapp... ", end="")
+t = time()
+
+zipapp.create_archive(src, dest_file)
+
+t = time()-t
+print(f"done!\a {int(t*1000)}ms")
